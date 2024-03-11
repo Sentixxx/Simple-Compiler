ce8f7db802bce001633a4aa107bc49349336c410
Sentixxx <1091441107@qq.com>
2024-03-11 22:35:56 +0800

    进一步封装了判断

--- Lexer/lexer.cpp
+++ Lexer/lexer.cpp
@@ -1,36 +1,54 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <algorithm>
+#include <vector>
 
 enum Token {
-    //文件结束符 eof
-    tok_eof = 1 ,
+    Error = 0,
+    // 文件结束符 eof
+    tok_eof = 1,
 
-    //标识符 identifier
-    tok_identifier = 2 ,
-    //常量 constant
-    tok_con = 3 ,
-    //保留字 reserved word
-    tok_rw = 4 ,
-    //运算符 operator
-    tok_op = 5 ,
-    //界符 delimiter
-    tok_del = 6 ,
-    //注释 comment
-    tok_comment = 7 ,
+    // 标识符 identifier
+    tok_identifier = 2,
+    // 常量 constant
+    tok_con = 3,
+    // 保留字 reserved word
+    tok_rw = 4,
+    // 运算符 operator
+    tok_op = 5,
+    // 界符 delimiter
+    tok_del = 6,
+    // 注释 comment
+    tok_comment = 7,
 };
 
-const std::string reservedWords[] = {
-"procedure", "def", "if", "else", "while", "call", "begin", "end", "and", "or"
+const std::string tok_str[] = {"Error",     "eof",           "identifier",
+                               "constant",  "reserved word", "operator",
+                               "delimiter", "comment"};
+
+bool comment_flag = false;
+
+const std::string reservedWords[] = {"procedure", "def",  "if",    "else",
+                                     "while",     "call", "begin", "end",
+                                     "and",       "or"
 
 };
-const std::string operators[] = {
-    "+" , "-" , "*" , "/" , "=", "<" , ">" , "<=" , ">=" , "<>", "=="
+const std::string operators[]     = {"+", "-",  "*",  "/",  "=", "<",
+                                     ">", "<=", ">=", "<>", "=="};
+
+struct TokenInfo
+{
+    int         line;
+    std::string lexeme;
+    Token       token;
 };
 
-int handle_alpha(std::string word) {
-    std::transform(word.begin() , word.end() , word.begin() , ::tolower);
+std::vector<TokenInfo> tokens;
+
+Token handleAlpha(std::string word)
+{
+    std::transform(word.begin(), word.end(), word.begin(), ::tolower);
     for (int i = 0; i < sizeof(reservedWords) / sizeof(reservedWords[0]); i++) {
         if (word == reservedWords[i]) {
             return tok_rw;
@@ -39,25 +57,35 @@ int handle_alpha(std::string word) {
     return tok_identifier;
 }
 
-int handle_operator(std::string word) {
+Token handleOperator(std::string word)
+{
     for (int i = 0; i < sizeof(operators) / sizeof(operators[0]); i++) {
         if (word == operators[i]) {
             return tok_op;
         }
     }
-    return 0;
+    return Error;
 }
 
-int sort(std::string word) {
+bool isAlpha(char c)
+{
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
+Token sort(std::string word)
+{
+    // std::cout << word[0];
     if (isdigit(word[0])) {
         return tok_con;
     }
-    else if (isalpha(word[0])) {
-        return handle_alpha(word);
+    else if (isAlpha(word[0]) || word[0] == '_') {
+        return handleAlpha(word);
     }
     else if (word[0] == '/') {
         if (word.size() == 1)
             return tok_op;
+        if (word[1] == '*')
+            comment_flag = true;
         return tok_comment;
     }
     else if (word[0] == '\'' || word[0] == '\"') {
@@ -67,61 +95,123 @@ int sort(std::string word) {
         return tok_con;
     }
     else {
-        return handle_operator(word);
+        // std::cout << "Error: Unrecognized token!\n";
+        return handleOperator(word);
     }
 }
 
-int Scanner() {
-    //打开输入源文件，设置行计数器为 0;
-    std::string inputFile = "input.txt";
+void saveToken(int line, std::string lexeme, Token token)
+{
+    tokens.push_back({line, lexeme, token});
+}
+
+int Scanner()
+{
+    // 打开输入源文件，设置行计数器为 0;
+    std::string inputFile  = "input.txt";
     std::string outputFile = "output.txt";
 
-    std::ifstream is = std::ifstream(inputFile);
-    std::ofstream os = std::ofstream(outputFile);
-    int lineCounter = 0;
+    std::ifstream is            = std::ifstream(inputFile);
+    std::ofstream os            = std::ofstream(outputFile);
+    int           lineCounter   = 0;
+    int           columnCounter = 0;
+    std::string   end_comment   = "";
+    char          c;
+    std::string   line;
+    std::string   word = "";
 
+    auto findToken = [&]() {
+        if (comment_flag) {
+            // std::cout << 1;
+            if (end_comment == "*" && c == '/') {
+                std::cout << 2;
+                comment_flag = false;
+                end_comment  = "";
+                saveToken(lineCounter, "*/", tok_comment);
+                return;
+            }
+            end_comment = c;
+        }
+        else {
+            if (c == '(' || c == ')' || c == ',' | c == ';') {
+                if (word != "")
+                    saveToken(lineCounter, word, sort(word));
+                word = "";
+                saveToken(lineCounter, std::string(1, c), tok_del);
+            }
 
-    std::string line;
-
-    while (is.peek() != EOF) {
-        std::getline(is , line);
-        std::cout << line << "\n";
-        //如果源文件没有结束，读入一行到 string，行计数+1，设置列计数器为 0
-        lineCounter++;
-        int columnCounter = 0;
-        if (!line.empty()) {
-            std::string word = "";
-            //std::cout << word << "\n";
-            for (char c : line) {
-                if (c == '(' || c == ')' || c == ',' | c == ';') {
-                    os << word << " " << sort(word) << "\n";
-                    word = "";
-                    os << c << " " << tok_del << "\n";
-                }
-                //
-                else if (c == ' ' || c == '\n' || c == '\t') {
-                    os << word << " " << sort(word) << "\n";
-                    word = "";
-                    continue;
+            else if (c == '+' || c == '-') {
+                if (word != "")
+                    saveToken(lineCounter, word, sort(word));
+                word = "";
+                saveToken(lineCounter, std::string(1, c), tok_op);
+            }
+            else if (c == '=' || c == '<' || c == '>' || c == '*' || c == '/') {
+                if (word == "*" || word == ">" || word == "/" || word == "<" ||
+                    word == "=") {
+                    word += c;
+                    saveToken(lineCounter, word, sort(word));
                 }
                 else {
-                    word += c;
-                    columnCounter++;
+                    if (word != "")
+                        saveToken(lineCounter, word, sort(word));
+                }
+            }
+            //
+            else if (c == ' ' || c == '\n' || c == '\t') {
+                if (word != "")
+                    saveToken(lineCounter, word, sort(word));
+                word = "";
+                return;
+            }
+            else {
+                word += c;
+                columnCounter++;
+            }
+        }
+    };
+
+    while (is.peek() != EOF) {
+        std::getline(is, line);
+        // std::cout << line << "\n";
+        //  如果源文件没有结束，读入一行到 string，行计数+1，设置列计数器为 0
+        lineCounter++;
+        columnCounter = 0;
+        if (!line.empty()) {
+            if (comment_flag) {
+                for (char x : line) {
+                    c = x;
+                    findToken();
+                }
+            }
+            else {
+                // std::cout << 4;
+                word = "";
+                // std::cout << word << "\n";
+                for (char x : line) {
+                    c = x;
+                    findToken();
                 }
             }
         }
         else {
             continue;
         }
-        //std::cout << lineCounter << "\n";
-        //output << line << std::endl;
+        // std::cout << lineCounter << "\n";
+        // output << line << std::endl;
     }
-
 
     return 1;
 }
 
-int main() {
+int main()
+{
     Scanner();
+
+    for (const auto& token : tokens) {
+        std::cout << "Line: " << token.line << "  ||  Lexeme:  " << token.lexeme
+                  << " ||  Token: " << tok_str[token.token] << "\n";
+    }
+
     return 0;
 }
