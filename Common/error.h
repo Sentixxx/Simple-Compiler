0fb5a1f31beb318aa97091f76000578e3ff17d84
sentixxx <1091441107@qq.com>
2024-04-15 17:04:07 +0800

    添加了error类型与处理

---
+++ Common/error.h
@@ -0,0 +1,11 @@
+//error.h
+#ifndef ERROR_H
+#define ERROR_H
+enum Error {
+    ok = 0 ,
+    invalid_word = 1 ,
+    missing_word = 2 ,
+    missing_match = 3 ,
+    unknown_error = 4 ,
+};
+#endif  // ERROR_H
--- Parser/parser.cpp
+++ Parser/parser.cpp
@@ -1,4 +1,5 @@
 #include "../Common/token.h"
+#include "../Common/error.h"
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -27,25 +28,119 @@ void read() {
     }
 }
 
-
-//因子->项->表达式->赋值语句->语句->语句列->程序
-
 class TokenNode {
 public:
     TokenInfo token;
-    TokenNode* next;
-
-    TokenNode(TokenInfo token) : token(token) , next(nullptr) {}
+    std::string name;
+    std::vector<TokenNode*> next;
+    TokenNode* parent;
+    TokenNode(TokenInfo token) : token(token) , name("Program") {}
+    TokenNode(TokenInfo token , std::string name) : token(token) , name(name) {}
 };
 
-TokenNode root = TokenNode(TokenInfo(0 , 0 , "program" , tok_comment));
 
+class Parser {
+private:
+    std::vector<TokenInfo> token_list;
+    TokenNode              root;
+    TokenNode* current;
+    int                    index;
 
+public:
+    Parser(std::vector<TokenInfo> token_list) : token_list(token_list) {
+        root = TokenNode(token_list[0]);
+        current = &root;
+        index = 0;
+    }
 
-void handleToken() {
+    void parseType() {
+        TokenNode* node = new TokenNode(token_list[index] , "TYPE");
+        current->next.push_back(node);
+        current = node;
+        index++;
+    }
 
-}
+    void parseCondStat() {
+        TokenNode* node = new TokenNode(token_list[index] , "COND_STAT");
+        current->next.push_back(node);
+        current = node;
+        index++;
+    }
+    //实验一的类型里面有string,实验二的文法里面没有提到，string要怎么处理？
+    void parse() {
+        while (index < token_list.size()) {
+            TokenInfo token = token_list[index];
+            if (token.token == tok_rw) {
+                if (token.lexeme == "int" || token.lexeme == "float" || token.lexeme == "string") {
+                    parseType();
+                }
+                else if (token.lexeme == "if") {
+                    parseCondStat();
+                }
+                else if (token.lexeme == "else") {
+                    parseIdtElse();
+                }
+                else if (token.lexeme == "while") {
+                    parseIdtWhile();
+                }
+                else if (token.lexeme == "return") {
+                    parseIdtReturn();
+                }
+                else if (token.lexeme == "break") {
+                    parseIdtBreak();
+                }
+                else if (token.lexeme == "and") {
+                    parseIdtAnd();
+                }
+                else {
+                    parseIdentifier();
+                }
+            }
+            else if (token.token == tok_del) {
+                if (token.lexeme == "{") {
+                    parseDelBlock();
+                }
+                else if (token.lexeme == ";") {
+                    parseDelSemicolon();
+                }
+                else if (token.lexeme == ",") {
+                    parseDelComma();
+                }
+                else if (token.lexeme == "(") {
+                    parseDelLeftParenthesis();
+                }
+                else if (token.lexeme == ")") {
+                    parseDelRightParenthesis();
+                }
+                else if (token.lexeme == "{") {
+                    parseDelLeftBrace();
+                }
+                else if (token.lexeme == "}") {
+                    parseDelRightBrace();
+                }
+                else {
+                    parseDelimiter();
+                }
+            }
+            else if (token.token == tok_bop) {
+                parseBasicOperator();
+            }
+            else if (token.token == tok_cop) {
+                parseCompareOperator();
+            }
+            else if (token.token == tok_aop) {
+                parseAssignOperator();
+            }
+            else if (token.token == tok_str) {
+                parseString();
+            }
+            else {
+                parseError();
+            }
+        }
+    }
+};
 
 int main() {
-
+    read();
 }
