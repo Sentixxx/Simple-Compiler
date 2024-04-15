880fcd83134b24074cd34510d855e6d6ac542ab0
sentixxx <1091441107@qq.com>
2024-04-15 14:24:22 +0800

    增加了token类的默认构造函数

--- Common/token.h
+++ Common/token.h
@@ -5,35 +5,37 @@
 #include <string>
 
 enum Token {
-    Error = 0,
-    // 标识符 identifier
-    tok_identifier = 1,
-    // 保留字 reserved word
-    tok_rw = 2,
-    // 字符串常量 string constant
-    tok_str = 3,
-    // 数字常量 number constant
-    tok_num = 4,
-    // 小数常量 float constant
-    tok_flt = 5,
-    // 界符 delimiter
-    tok_del = 6,
-    // 四则运算符 basic operator
-    tok_bop = 7,
-    // 比较运算符 compare operator
-    tok_cop = 8,
-    // 赋值运算符 assign operator
-    tok_aop = 9,
-    // 注释 comment
-    tok_comment = 10,
+	Error = 0 ,
+	// 标识符 identifier
+	tok_identifier = 1 ,
+	// 保留字 reserved word
+	tok_rw = 2 ,
+	// 字符串常量 string constant
+	tok_str = 3 ,
+	// 数字常量 number constant
+	tok_num = 4 ,
+	// 小数常量 float constant
+	tok_flt = 5 ,
+	// 界符 delimiter
+	tok_del = 6 ,
+	// 四则运算符 basic operator
+	tok_bop = 7 ,
+	// 比较运算符 compare operator
+	tok_cop = 8 ,
+	// 赋值运算符 assign operator
+	tok_aop = 9 ,
+	// 注释 comment
+	tok_comment = 10 ,
 };
 
 class TokenInfo {
-  public:
-    int         line;
-    int         column;
-    std::string lexeme;
-    Token       token;
+public:
+	int         line;
+	int         column;
+	std::string lexeme;
+	Token       token;
+	TokenInfo() : line(0) , column(0) , lexeme("") , token(Error) {}
+	TokenInfo(int line , int column , std::string lexeme , Token token) : line(line) , column(column) , lexeme(lexeme) , token(token) {}
 };
 
 #endif  // TOKEN_H
--- Parser/parser.cpp
+++ Parser/parser.cpp
@@ -3,15 +3,14 @@
 #include <iostream>
 #include <vector>
 
-std::string   inputFile  = "../Data/lexer_token.out";
+std::string   inputFile = "../Data/lexer_token.out";
 std::string   outputFile = "../Data/parser_list.out";
-std::ifstream is         = std::ifstream(inputFile);
-std::ofstream os         = std::ofstream(outputFile);
+std::ifstream is = std::ifstream(inputFile);
+std::ofstream os = std::ofstream(outputFile);
 
 bool read_flag = true;
 
-TokenInfo readToken(std::ifstream& in)
-{
+TokenInfo readToken(std::ifstream& in) {
     if (in.eof())
         read_flag = false;
     TokenInfo token;
@@ -22,5 +21,31 @@ TokenInfo readToken(std::ifstream& in)
 }
 
 std::vector<TokenInfo> token_list;
+void read() {
+    while (read_flag) {
+        token_list.push_back(readToken(is));
+    }
+}
 
-int main() {}
+
+//因子->项->表达式->赋值语句->语句->语句列->程序
+
+class TokenNode {
+public:
+    TokenInfo token;
+    TokenNode* next;
+
+    TokenNode(TokenInfo token) : token(token) , next(nullptr) {}
+};
+
+TokenNode root = TokenNode(TokenInfo(0 , 0 , "program" , tok_comment));
+
+
+
+void handleToken() {
+
+}
+
+int main() {
+
+}
