44abc08f8c5cbe045b26b2a8a7fb9d3f02c5cc2d
Sentixxx <1091441107@qq.com>
2024-04-16 21:49:55 +0800

    seperate NT and T set

--- Parser/grammar.cpp
+++ Parser/grammar.cpp
@@ -1,7 +1,14 @@
-#include <bits/stdc++.h>
 #include "grammar.h"
+#include <bits/stdc++.h>
 
-int main() {
+int main()
+{
     Grammar G;
+    G.findTerminals();
+    for (auto i : G.T)
+        std::cout << i << std::endl;
+    std::cout << "-------------------" << std::endl;
+    for (auto i : G.NT)
+        std::cout << i << std::endl;
     std::cout << G.hasLeftRecursion();
 }
--- Parser/grammar.h
+++ Parser/grammar.h
@@ -1,276 +1,119 @@
-//grammar.h
+// grammar.h
 #ifndef GRAMMAR_H
 #define GRAMMAR_H
+#include <set>
+#include <string>
 #include <unordered_map>
 #include <vector>
-#include <string>
-#include <set>
-#define EPSILON 135ru9heiugfha 
+#define EPSILON r3315u9heiugfha
 class Grammar {
-public:
-    std::unordered_map <std::string , std::vector<std::vector<std::string>>> list;
-    std::set<std::string> T;
-    Grammar() {
+  public:
+    std::unordered_map<std::string, std::vector<std::vector<std::string>>> list;
+    std::set<std::string>                                                  T;
+    std::set<std::string>                                                  NT;
+    Grammar()
+    {
         /***程序：由0个或多个全局变量或函数组成***/
-        list["Program"] = {
-            std::vector<std::string> {
-                "ExtDefList"
-            }
-        };
-        list["ExtDefList"] = {
-            std::vector<std::string> {
-                "ExtDef", "ExtDefList"
-            } ,
-            std::vector < std::string> {
-                "EPSILON"
-            }
-        };
-        list["ExtDef"] = { std::vector<std::string> {
-            "TYPE" , "id" , "ExtDefRest"
-            } ,
-        std::vector<std::string> {
-            "TYPE" , "FunDec" , "CompSt"
-            }
-        };
-        //实验一加入了string类
-        list["TYPE"] = {
-            std::vector<std::string> {
-            "int" , "float" , "string"
-            }
-        };
+        list["Program"]    = {std::vector<std::string>{"ExtDefList"}};
+        list["ExtDefList"] = {std::vector<std::string>{"ExtDef", "ExtDefList"},
+                              std::vector<std::string>{"EPSILON"}};
+        list["ExtDef"] = {std::vector<std::string>{"TYPE", "id", "ExtDefRest"},
+                          std::vector<std::string>{"TYPE", "FunDec", "CompSt"}};
+        list["ExtDefRest"] = {std::vector<std::string>{",", "id", "ExtDefRest"},
+                              std::vector<std::string>{"EPSILON"}};
+        // 实验一加入了string类
+        list["TYPE"] = {std::vector<std::string>{"int", "float", "string"}};
         /***函数头定义***/
-        list["FunDec"] = {
-            std::vector<std::string> {
-                "id" , "(" , "VarList" , ")"
-            }
-        };
+        list["FunDec"]  = {std::vector<std::string>{"id", "(", "VarList", ")"}};
         list["VarList"] = {
-            std::vector<std::string> {
-                "TYPE" , "id", "VarListRest"
-            } ,
-            std::vector < std::string> {
-                "EPSILON"
-            }
-        };
+            std::vector<std::string>{"TYPE", "id", "VarListRest"},
+            std::vector<std::string>{"EPSILON"}};
         list["VarListRest"] = {
-            std::vector < std::string> {
-                "," , "TYPE" , "id" , "VarListRest"
-                } ,
-            std::vector < std::string> {
-                "EPSILON"
-                }
-        };
+            std::vector<std::string>{",", "TYPE", "id", "VarListRest"},
+            std::vector<std::string>{"EPSILON"}};
         /*** CompSt：复合语句块***/
-        list["CompSt"] = {
-            std::vector < std::string> {
-                "{" , "StmtList" , "}"
-            },
-            std::vector < std::string> {
-                "EPSILON"
-            }
-        };
-        list["StmtList"] = {
-            std::vector < std::string> {
-                "Stmt" , "StmtList"
-            },
-            std::vector < std::string> {
-                "EPSILON"
-            }
-        };
+        list["CompSt"]   = {std::vector<std::string>{"{", "StmtList", "}"},
+                            std::vector<std::string>{"EPSILON"}};
+        list["StmtList"] = {std::vector<std::string>{"Stmt", "StmtList"},
+                            std::vector<std::string>{"EPSILON"}};
         /*** Stmt：语句***/
-        list["Stmt"] = {
-            std::vector < std::string> {
-                "ConditionalStmt" , "LoopStmt" , "CallStmt", "AssignmentStmt" , "ReturnStmt" , "BreakStmt" , "LocalVariableDeclaration" , "CompSt" , ";"
-            }
-        };
+        list["Stmt"] = {std::vector<std::string>{
+            "ConditionalStmt", "LoopStmt", "CallStmt", "AssignmentStmt",
+            "ReturnStmt", "BreakStmt", "LocalVariableDeclaration", "CompSt",
+            ";"}};
         /***局部变量定义语句***/
-        list["LocalVariableDeclaration"] = {
-            std::vector<std::string> {
-                "TYPE" , "id" , "LocalVariableDeclarationRest", ";"
-            }
-        };
+        list["LocalVariableDeclaration"]     = {std::vector<std::string>{
+            "TYPE", "id", "LocalVariableDeclarationRest", ";"}};
         list["LocalVariableDeclarationRest"] = {
-            std::vector<std::string> {
-                "," , "id" , "LocalVariableDeclarationRest"
-            } ,
-            std::vector < std::string> {
-                "EPSILON"
-            }
-        };
+            std::vector<std::string>{",", "id", "LocalVariableDeclarationRest"},
+            std::vector<std::string>{"EPSILON"}};
         /***函数调用语句***/
         list["CallStmt"] = {
-            std::vector<std::string> {
-                "id" , "(" , "ArgList" , ")" , ";"
-            }
-        };
+            std::vector<std::string>{"id", "(", "ArgList", ")", ";"}};
         /***实参列表***/
-        list["ArgList"] = {
-            std::vector<std::string> {
-                "Exp" , "ArgListRest"
-            } ,
-            std::vector < std::string> {
-                "EPSILON"
-            }
-        };
+        list["ArgList"]     = {std::vector<std::string>{"Exp", "ArgListRest"},
+                               std::vector<std::string>{"EPSILON"}};
         list["ArgListRest"] = {
-            std::vector<std::string> {
-                "," , "Exp" , "ArgListRest"
-            } ,
-            std::vector < std::string> {
-                "EPSILON"
-            }
-        };
+            std::vector<std::string>{",", "Exp", "ArgListRest"},
+            std::vector<std::string>{"EPSILON"}};
         /***赋值语句***/
         list["AssignmentStmt"] = {
-            std::vector<std::string> {
-                "id" , "=" , "Expression" , ";"
-            }
-        };
+            std::vector<std::string>{"id", "=", "Exp", ";"}};
         /***条件语句***/
-        list["ConditionalStmt"] = {
-            std::vector<std::string> {
-                "if" , "(" , "ConditionalExp" , ")" , "Stmt" , "ConditionalStmtRest"
-            }
-        };
-        list["ConditionalStmtRest"] = {
-            std::vector<std::string> {
-                "else" , "Stmt"
-            } ,
-            std::vector < std::string> {
-                "EPSILON"
-            }
-        };
+        list["ConditionalStmt"]     = {std::vector<std::string>{
+            "if", "(", "ConditionalExp", ")", "Stmt", "ConditionalStmtRest"}};
+        list["ConditionalStmtRest"] = {std::vector<std::string>{"else", "Stmt"},
+                                       std::vector<std::string>{"EPSILON"}};
         /***循环语句***/
-        list["LoopStmt"] = {
-            std::vector<std::string> {
-                "while" , "(" , "ConditionalExp" , ")" , "Stmt"
-            }
-        };
+        list["LoopStmt"] = {std::vector<std::string>{
+            "while", "(", "ConditionalExp", ")", "Stmt"}};
         /***返回语句***/
         list["ReturnStmt"] = {
-            std::vector<std::string> {
-                "return" , "ReturnStmtRest" , ";"
-            }
-        };
-        list["ReturnStmtRest"] = {
-            std::vector<std::string> {
-                "Exp"
-            },
-            std::vector<std::string> {
-                "EPSILON"
-            }
-        };
+            std::vector<std::string>{"return", "ReturnStmtRest", ";"}};
+        list["ReturnStmtRest"] = {std::vector<std::string>{"Exp"},
+                                  std::vector<std::string>{"EPSILON"}};
         /***中止语句***/
-        list["BreakStmt"] = {
-            std::vector<std::string> {
-                "break", ";"
-            }
-        };
+        list["BreakStmt"] = {std::vector<std::string>{"break", ";"}};
 
         /***四则表达式***/
-        list["Exp"] = {
-            std::vector<std::string> {
-                "Term" , "ExpRest"
-            }
-        };
-        list["ExpRest"] = {
-            std::vector<std::string> {
-                "+" , "Term" , "ExpRest"
-            },
-            std::vector<std::string> {
-                "-" , "Term" , "ExpRest"
-            },
-            std::vector<std::string> {
-                "EPSILON"
-            }
-        };
-        list["Term"] = {
-            std::vector<std::string> {
-                "Factor" , "TermRest"
-            }
-        };
-        list["TermRest"] = {
-            std::vector<std::string> {
-                "*" , "Factor" , "TermRest"
-            },
-            std::vector<std::string> {
-                "/" , "Factor" , "TermRest"
-            },
-            std::vector<std::string> {
-                "EPSILON"
-            }
-        };
-        list["Factor"] = {
-            std::vector<std::string> {
-                "id"
-            },
-            std::vector<std::string> {
-                "INTC"
-            },
-            std::vector<std::string> {
-                "DECI"
-            },
-            std::vector<std::string> {
-                "(" , "Exp" , ")"
-            }
-        };
+        list["Exp"]      = {std::vector<std::string>{"Term", "ExpRest"}};
+        list["ExpRest"]  = {std::vector<std::string>{"+", "Term", "ExpRest"},
+                            std::vector<std::string>{"-", "Term", "ExpRest"},
+                            std::vector<std::string>{"EPSILON"}};
+        list["Term"]     = {std::vector<std::string>{"Factor", "TermRest"}};
+        list["TermRest"] = {std::vector<std::string>{"*", "Factor", "TermRest"},
+                            std::vector<std::string>{"/", "Factor", "TermRest"},
+                            std::vector<std::string>{"EPSILON"}};
+        list["Factor"]   = {std::vector<std::string>{"id"},
+                            std::vector<std::string>{"INTC"},
+                            std::vector<std::string>{"DECI"},
+                            std::vector<std::string>{"(", "Exp", ")"}};
 
         /***条件表达式***/
         list["ConditionalExp"] = {
-            std::vector<std::string> {
-                "RelationExp" , "ConditionalExpRest"
-            }
-        };
+            std::vector<std::string>{"RelationExp", "ConditionalExpRest"}};
         list["ConditionalExpRest"] = {
-            std::vector<std::string> {
-                "or" , "RelationExp" , "ConditionExpRest"
-            },
-            std::vector<std::string> {
-                "EPSILON"
-            }
-        };
+            std::vector<std::string>{"or", "RelationExp", "ConditionalExpRest"},
+            std::vector<std::string>{"EPSILON"}};
         list["RelationExp"] = {
-            std::vector<std::string> {
-                "CompExp" , "RelationExpRest"
-            }
-        };
+            std::vector<std::string>{"CompExp", "RelationExpRest"}};
         list["RelationExpRest"] = {
-            std::vector<std::string> {
-                "and" , "CompExp" , "RelationExpRest"
-            },
-            std::vector<std::string> {
-                "EPSILON"
-            }
-        };
-        list["CompExp"] = {
-            std::vector<std::string> {
-                "Exp" , "CmpOp" , "Exp"
-            }
-        };
-        list["CmpOp"] = {
-            std::vector<std::string> {
-                "<"
-            },
-            std::vector<std::string> {
-                "<="
-            },
-            std::vector<std::string> {
-                ">"
-            },
-            std::vector<std::string> {
-                ">="
-            },
-            std::vector<std::string> {
-                "=="
-            },
-            //实验一将<>修改为了!=
-            std::vector<std::string> {
-                "!="
-            }
-        };
+            std::vector<std::string>{"and", "CompExp", "RelationExpRest"},
+            std::vector<std::string>{"EPSILON"}};
+        list["CompExp"] = {std::vector<std::string>{"Exp", "CmpOp", "Exp"}};
+        list["CmpOp"]   = {
+            std::vector<std::string>{"<"}, std::vector<std::string>{"<="},
+            std::vector<std::string>{">"}, std::vector<std::string>{">="},
+            std::vector<std::string>{"=="},
+            // 实验一将<>修改为了!=
+            std::vector<std::string>{"!="}};
+
+        findTerminals();
+        findNonTerminals();
     }
-    bool hasLeftRecursion() {
-        for (auto& [key , value] : list) {
+    bool hasLeftRecursion()
+    {
+        for (auto& [key, value] : list) {
             for (auto& v : value) {
                 if (v[0] == key) {
                     return true;
@@ -280,16 +123,26 @@ public:
         return false;
     }
 
-    //判断终结符并加入集合T
-    void findTerminals() {
-        for (auto& [key , value] : list) {
+    // 判断终结符并加入集合T
+    void findTerminals()
+    {
+        for (auto& [key, value] : list) {
             for (auto& production : value) {
                 for (auto& symbol : production) {
-                    if (symbol != "EPSILON" && symbol != key) {
+                    if (symbol != "EPSILON" &&
+                        list.find(symbol) == list.end()) {
                         T.insert(symbol);
                     }
                 }
             }
+        }
+    }
+
+    void findNonTerminals()
+    {
+        for (auto& [key, value] : list) {
+            if (T.find(key) == T.end())
+                NT.insert(key);
         }
     }
 };
