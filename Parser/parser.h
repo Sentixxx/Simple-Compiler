10b35e84f6f3297598645a4bdda61a4b8c124690
sentixxx <1091441107@qq.com>
2024-04-24 19:17:09 +0800

    first commit

--- Parser/parser.h
+++ Parser/parser.h
@@ -12,13 +12,12 @@
 #include <string>
 
 class TopParser : public Module {
-  private:
-    int         i    = 0;
+private:
+    int         i = 0;
     int         maxi = 0;
-    TokenList&  TL;
-    std::string handleToken()
-    {
-        std::pair<std::string, std::string> token = TL.getTokenType(i);
+    TokenList& TL;
+    std::string handleToken() {
+        std::pair<std::string , std::string> token = TL.getTokenType(i);
         if (token.first == "number")
             return "INTC";
         else if (token.first == "float")
@@ -26,442 +25,192 @@ class TopParser : public Module {
         else if (token.first == "identifier") {
             return "id";
         }
+        else if ((token.second == "float" || token.second == "string" || token.second == "int") && token.first == "reserved word") {
+            return "TYPE";
+        }
+        else if (token.first == "compare operator") {
+            return "CmOp";
+        }
         else {
             return token.second;
         }
     }
-    void next()
-    {
-        maxi = std::max(i, maxi);
-        if (i == TL.tok_lis.size() - 1) {
-            std::cerr << "reach the end of file!\n";
+
+    void next() {
+        if (i == TL.tok_lis.size()) {
+            throw "Error: Unexpected end of file\n";
+            return;
         }
-        else {
-            std::cout << "try handle: " << handleToken() << "\n";
-            i++;
-        }
-    }
-    void back()
-    {
-        i--;
-        std::cerr << "Fail handle: " << handleToken() << "\n";
+        i++;
     }
 
-  public:
-    TopParser(TokenList& TL) : TL(TL)
-    {
-        this->inputFile  = "../Data/lexer_token.out";
-        this->outputFile = "../Data/parser_list.out";
-        this->is         = std::ifstream(inputFile);
-        this->os         = std::ofstream(outputFile);
-    }
-    bool parseTYPE()
-    {
-        // std::cout << "parseTYPE --> ";
-        if (handleToken() == "int" || handleToken() == "float" ||
-            handleToken() == "string") {
-            return true;
-        }
-        return false;
-    }
-    bool parseCmpOp()
-    {
-        if (handleToken() == "==" || handleToken() == "!=" ||
-            handleToken() == "<" || handleToken() == ">" ||
-            handleToken() == "<=" || handleToken() == ">=") {
-            return true;
-        }
-        return false;
-    }
-    bool parseExtDefRest()
-    {
-        if (handleToken() == ",") {
+    void varList() {
+        if (handleToken() == "TYPE") {
             next();
             if (handleToken() == "id") {
                 next();
-                if (parseExtDefRest())
-                    return true;
-                back();
-            }
-            back();
-        }
-        return true;
-    }
-    bool parseVarListRest()
-    {
-        if (handleToken() == ",") {
-            next();
-            if (parseTYPE()) {
-                next();
-                if (handleToken() == "id") {
+                if (handleToken() == ",") {
                     next();
-                    if (parseVarListRest())
-                        return true;
-                    back();
+                    if (handleToken() == "TYPE") {
+                        varList();
+                    }
+                    else {
+                        throw "Error: Expected id, but find" + handleToken() + "\n";
+                    }
                 }
-                back();
+                else {
+                    return;
+                }
             }
-            back();
+            else {
+                throw "Error: Expected id , but find" + handleToken() + "\n";
+            }
         }
-        return true;
+        else {
+            return;
+        }
     }
-    bool parseVarList()
-    {
-        if (parseTYPE()) {
+
+    void FunDec() {
+        //(
+        next();
+        varList();
+        if (handleToken() == ")") {
             next();
-            if (handleToken() == "id") {
-                next();
-                if (parseVarListRest())
-                    return true;
-                back();
-            }
-            back();
         }
-        return true;
+        else {
+            throw "Error: Expected ) , but find" + handleToken() + "\n";
+        }
     }
-    bool parseFunDec()
-    {
+
+    void Stmt();
+
+    void ConditionalExp() {
+
+    }
+
+    void ConditionalStmt() {
+        next();
         if (handleToken() == "(") {
             next();
-            if (parseVarList())
-                if (handleToken() == ")") {
+            ConditionalExp();
+            if (handleToken() == ")") {
+                next();
+                Stmt();
+                if (handleToken() == "else") {
                     next();
-                    return true;
+                    Stmt();
                 }
-            back();
+            }
+            else {
+                throw "Error: Expected ) , but find" + handleToken() + "\n";
+            }
         }
-        return false;
     }
 
-    bool parseFactor()
-    {
-        if (handleToken() == "id" || handleToken() == "INTC" ||
-            handleToken() == "DECI") {
-            next();
-            return true;
-        }
-        else {
-            if (handleToken() == "(") {
-                next();
-                if (parseExp())
-                    if (handleToken() == ")") {
-                        next();
-                        return true;
-                    }
-                back();
-            }
-        }
-        return false;
-    }
-    bool parseTerm()
-    {
-        if (parseFactor()) {
-            while (handleToken() == "*" || handleToken() == "/") {
-                next();
-                if (!parseFactor()) {
-                    back();
-                    break;
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-    bool parseExpRest()
-    {
-        return false;
-    }
-    bool parseExp()
-    {
-        if (parseTerm()) {
-            while (handleToken() == "+" || handleToken() == "-") {
-                next();
-                if (!parseTerm()) {
-                    back();
-                    break;
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-    bool parseCompExp()
-    {
-        if (parseExp()) {
-            if (parseCmpOp()) {
-                next();
-                if (parseExp())
-                    return true;
-                back();
-            }
-        }
-        return false;
-    }
-    bool parseRelationExp()
-    {
-        if (parseCompExp()) {
-            while (handleToken() == "and") {
-                next();
-                if (!parseRelationExp()) {
-                    back();
-                    break;
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-    bool parseConditionalExp()
-    {
-        if (parseRelationExp()) {
-            while (handleToken() == "or") {
-                next();
-                if (!parseRelationExp()) {
-                    back();
-                    break;
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-    bool parseConditionalStmt()
-    {
-        if (handleToken() == "if") {
-            next();
-            if (handleToken() == "(") {
-                next();
-                if (parseConditionalExp())
-                    if (handleToken() == ")") {
-                        next();
-                        if (parseStmt()) {
-                            if (handleToken() == "else") {
-                                next();
-                                if (parseStmt())
-                                    return true;
-                                back();
-                            }
-                            return true;
-                        }
-                        back();
-                    }
-                back();
-            }
-            back();
-        }
-        return false;
-    }
-    bool parseActParamList()
-    {
-        if (parseExp()) {
-            while (handleToken() == ",") {
-                next();
-                if (!parseExp()) {
-                    back();
-                    break;
-                }
-            }
-            return true;
-        }
-        return true;
-    }
-    bool parseCallStmt()
-    {
-        if (handleToken() == "id") {
-            next();
-            if (handleToken() == "(") {
-                next();
-                if (parseActParamList()) {
-                    if (handleToken() == ")") {
-                        next();
-                        if (handleToken() == ";") {
-                            next();
-                            return true;
-                        }
-                        back();
-                    }
-                }
-                back();
-            }
-            back();
-        }
-        return false;
-    }
-    bool parseLoopStmt()
-    {
-        if (handleToken() == "while") {
-            next();
-            if (handleToken() == "(") {
-                next();
-                if (parseConditionalExp()) {
-                    if (handleToken() == ")") {
-                        next();
-                        if (parseStmt()) {
-                            return true;
-                        }
-                        back();
-                    }
-                }
-                back();
-            }
-            back();
-        }
-        return false;
-    }
-    bool parseAssignmentStmt()
-    {
-        if (handleToken() == "id") {
-            next();
-            if (handleToken() == "=") {
-                next();
-                if (parseExp())
-                    if (handleToken() == ";") {
-                        next();
-                        return true;
-                    }
-                back();
-            }
-            back();
-        }
-        return false;
-    }
-    bool parseReturnStmt()
-    {
-        if (handleToken() == "return") {
-            next();
-            if (parseExp())
-                if (handleToken() == ";") {
-                    next();
-                    return true;
-                }
-            return true;
-        }
-        return false;
-    }
-    bool parseBreakStmt()
-    {
-        if (handleToken() == "break") {
-            next();
-            if (handleToken() == ";") {
-                next();
-                return true;
-            }
-            back();
-        }
-        return false;
-    }
-    bool parseLocalVariableDeclaration()
-    {
-        if (parseTYPE()) {
-            next();
-            if (handleToken() == "id") {
-                next();
-                while (handleToken() == ",") {
-                    next();
-                    if (handleToken() == "id") {}
-                    else {
-                        back();
-                        break;
-                    }
-                }
-                if (handleToken() == ";") {
-                    next();
-                    return true;
-                }
-                back();
-            }
-            back();
-        }
-        return false;
-    }
-    bool parseStmt()
-    {
+    void Stmt() {
         if (handleToken() == ";") {
             next();
-            return true;
+            return;
         }
-        if (parseConditionalStmt() || parseLoopStmt() || parseCallStmt() ||
-            parseAssignmentStmt() || parseReturnStmt() || parseBreakStmt() ||
-            parseLocalVariableDeclaration() || parseCompSt()) {
-            return true;
+        else if (handleToken() == "if") {
+            ConditionalStmt();
         }
-        return false;
-    }
-    bool parseStmtList()
-    {
-        if (parseStmt()) {
-            if (parseStmtList())
-                return true;
+        else {
+            throw "Error: Expected ( , but find" + handleToken() + "\n";
         }
-        return true;
-    }
-    bool parseCompSt()
-    {
-        if (handleToken() == "{") {
-            next();
-            if (parseStmtList()) {
-                if (handleToken() == "}") {
-                    next();
-                    return true;
-                }
-            }
-            back();
-        }
-        return false;
     }
 
-    bool parseExtDef()
-    {
-        // std::cout << "parseExtDef --> ";
-        if (parseTYPE()) {
+
+
+
+    void StmtList() {
+        if (handleToken() == ";" || handleToken() == "if" || handleToken() == "while" || handleToken() == "return" || handleToken() == "id" || handleToken() == "{" || handleToken() == "TYPE" || handleToken() == "break") {
+            Stmt();
+            StmtList();
+        }
+        else {
+            return;
+        }
+    }
+
+    void CompSt() {
+        if (handleToken() == "{") {
             next();
-            if (handleToken() == "id") {
+            StmtList();
+            if (handleToken() == "}") {
                 next();
-                if (parseExtDefRest())
-                    if (handleToken() == ";") {
-                        next();
-                        return true;
-                    }
-                if (parseFunDec())
-                    if (parseCompSt())
-                        return true;
-                back();
+                return;
             }
-            back();
-        }
-        return false;
-    }
-    bool parseExtDefList()
-    {
-        // std::cout << "parseExtDefList --> ";
-        if (parseExtDef()) {
-            if (parseExtDefList())
-                return true;
             else {
-                return false;
+                throw "Error: Expected } , but find" + handleToken() + "\n";
             }
         }
-        return true;
-    }
-    bool parseProgram()
-    {
-        // std::cout << "parseProgram --> ";
-        if (parseExtDefList()) {
-            return true;
+        else {
+            throw "Error: Expected { , but find" + handleToken() + "\n";
         }
-        return false;
     }
-    void lparse()
-    {
+
+    void ExtDef() {
+        //TYPE
+        next();
+        if (handleToken() == "id") {
+            next();
+            if (handleToken() == "(") {
+                FunDec();
+                CompSt();
+            }
+            else if (handleToken() == ",") {
+                ExtDef();
+            }
+            else if (handleToken() == ";") {
+                return;
+            }
+            else {
+                throw "Error: Expected ( or , or ; , but find" + handleToken() + "\n";
+            }
+        }
+        else {
+            throw "Error: Expected id , but find" + handleToken() + "\n";
+        }
+    }
+
+    void ExtDefList() {
+        if (handleToken() == "TYPE") {
+            ExtDef();
+            ExtDefList();
+        }
+        else {
+            //EPSILON
+            return;
+        }
+    }
+
+    void Program() {
+        while (i < TL.tok_lis.size()) {
+            ExtDefList();
+        }
+    }
+
+
+public:
+    TopParser(TokenList& TL) : TL(TL) {
+        this->inputFile = "../Data/lexer_token.out";
+        this->outputFile = "../Data/parser_list.out";
+        this->is = std::ifstream(inputFile);
+        this->os = std::ofstream(outputFile);
+    }
+    void lparse() {
         try {
-            if (parseProgram() && i == TL.tok_lis.size() - 1) {
+            Program();
+            if (i == TL.tok_lis.size() - 1) {
                 std::cout << "YES";
                 return;
             }
             else {
                 std::cout << "NO\n";
                 std::cerr << "Error at " << TL.tok_lis[maxi].line << ":"
-                          << TL.tok_lis[maxi].column << "\n";
+                    << TL.tok_lis[maxi].column << "\n";
             }
         }
         catch (const char* e) {
